d2e51bda269cb61d7f545a544462817532a268da
Naman Sharma <namansharma18899@gmail.com>
2024-12-17 22:29:59 +0530

    Donegit add rlimiter-per-user/limit.go need to write some test for that....

--- rlimiter-per-user/limit.go
+++ rlimiter-per-user/limit.go
@@ -2,36 +2,45 @@ package main
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
+	"sync"
 	"time"
 
 	"golang.org/x/time/rate"
 )
 
 type Client struct {
-	limter      *rate.Limiter
+	limiter     *rate.Limiter
 	lastVisited time.Time
 }
 
 // We need to have a map of clients
-
 // globalMapOfClients := make(map[string]Client)
 
-globalMapOfClients := make(map[string]Client)
+var clients = make(map[string]*Client)
 
-
-func perUserRateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
-	limiter := rate.NewLimiter(2, 4)
+func perUserRateLimiter(next func(writer http.ResponseWriter, request *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		clientIp, _, err := net.SplitHostPort(r.RemoteAddr)
+		var mu sync.Mutex
+		if err != nil {
+			return
+		}
+		/* We need to make this thread safe caus clients' is a global
+		map & can be accessed by other calls.. */
+		mu.Lock()
+		if _, exists := clients[clientIp]; !exists {
+			clients[clientIp] = &Client{limiter: rate.NewLimiter(2, 40)}
+		}
+		clients[clientIp].lastVisited = time.Now()
+		mu.Unlock()
+		if !clients[clientIp].limiter.Allow() {
 			message := Message{Status: "Request Failed",
 				Body: "Too many requests",
 			}
 			w.WriteHeader(http.StatusTooManyRequests)
 			json.NewEncoder(w).Encode(&message)
-			return
-		} else {
-			next(w, r)
 		}
 	})
 }
